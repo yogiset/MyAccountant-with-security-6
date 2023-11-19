0fe4dae76f3bacc10f2da95780223da3492d6871
yogiset <the3verlastin9@gmail.com>
2023-11-20 05:36:49 +0700

    Added dto for authmessage but looks like i didnt use it

---
+++ src/main/java/com/accountant/MyAccountant/dto/AuthMessage.java
@@ -0,0 +1,12 @@
+package com.accountant.MyAccountant.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthMessage {
+    private String message;
+}

45967173bb4c4f081f51a3311fffeae620db3ded
yogiset <the3verlastin9@gmail.com>
2023-11-20 05:39:44 +0700

    nothing changes just trying something

--- src/main/java/com/accountant/MyAccountant/jwt/JwtUtil.java
+++ src/main/java/com/accountant/MyAccountant/jwt/JwtUtil.java
@@ -1,11 +1,15 @@
 package com.accountant.MyAccountant.jwt;
 
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,56 +19,61 @@ import java.util.function.Function;
 public class JwtUtil {
 
     private final SecretKey secretKey;
-    private final long jwtExpirationInMillis = 1000 * 60 * 60 * 10; // 10 hours
+    private static final long jwtExpirationInMillis = 1000 * 60 * 60 * 10; // 10 hours
 
     public JwtUtil() {
         // Generate a secure key for HS256 algorithm in the constructor
         this.secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
     }
-
-    public String extractUsername(String token) {
-        return extractClaims(token, Claims::getSubject);
+    public String extractuserName(String token) {
+        return extractClaim(token, Claims::getSubject);
     }
 
-    public Date extractExpiration(String token) {
-        return extractClaims(token, Claims::getExpiration);
-    }
-
-    public <T> T extractClaims(String token, Function<Claims, T> claimsResolver) {
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
 
-    public Claims extractAllClaims(String token) {
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
-    }
-
-    private Boolean isTokenExpired(String token) {
-        return extractExpiration(token).before(new Date());
-    }
-
     public String generateToken(String username, String role,String name) {
-        Map<String, Object> claims = new HashMap<>();
-        claims.put("role", role);
-        claims.put("name", name);
-        return createToken(claims, username);
+        Map<String, Object> Claims = new HashMap<>();
+        Claims.put("role", role);
+        Claims.put("name", name);
+        return createToken(Claims,username);
     }
-
-    private String createToken(Map<String, Object> claims, String subject) {
+    private String createToken(Map<String, Object> claims, String email) {
         return Jwts.builder()
                 .setClaims(claims)
-                .setSubject(subject)
+                .setSubject(email)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationInMillis))
                 .signWith(secretKey, SignatureAlgorithm.HS256).compact();
     }
 
+
     public Boolean validateToken(String token, UserDetails userDetails) {
-        final String username = extractUsername(token);
+        final String username = extractuserName(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token,Claims::getExpiration);
+    }
+
+    public Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+    }
     public String extractRole(String token) {
-        return extractClaims(token, claims -> (String) claims.get("role"));
+        return extractClaim(token, claims -> (String) claims.get("role"));
     }
 
     public Claims validateAndExtractClaims(String token) {
